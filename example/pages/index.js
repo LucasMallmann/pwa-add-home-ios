719b7c70057809a91fd068f541b59ede86f889ab
lucasmallmann <lucasmallmann76@gmail.com>
2020-05-28 12:27:56 -0300

    refactor: updated example

--- example/pages/index.js
+++ example/pages/index.js
@@ -7,24 +7,29 @@ const Modal = dynamic(() => import('pwa-add-home-ios'), {
   ssr: false,
 });
 
-const Home = () => (
-  <div className="container">
-    <div className="content">
-      <h1>Ola</h1>
-    </div>
-    <Modal
-      title="Adicionar banana à página inicial"
-      image={banana}
-      onClickBanner={() => alert('hey bro')}
-    />
+const Home = () => {
+  return (
+    <>
+      <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+      <div className="container">
+        <div className="content">
+          <h1>Ola</h1>
+        </div>
+        <Modal
+          title="Adicionar banana à página inicial"
+          image={banana}
+          onClickBanner={() => alert('hey bro')}
+        />
 
-    <style jsx>{`
-      .content {
-        height: 2000px;
-        background-color: red;
-      }
-    `}</style>
-  </div>
-);
+        <style jsx>{`
+          .content {
+            height: 2000px;
+            background-color: red;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+};
 
 export default Home;
