be8e6e81db014d1ff33cad64baf2feebd619c8ce
lucasmallmann <lucasmallmann76@gmail.com>
2020-05-28 10:51:01 -0300

    feat: added nextjs example

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 
-import { ExampleComponent } from 'test';
+import banana from '../public/banana.jpg';
+
+const Modal = dynamic(() => import('pwa-add-home-ios'), {
+  ssr: false,
+});
 
 const Home = () => (
   <div className="container">
-    <ExampleComponent text="Oi" />
+    <div className="content">
+      <h1>Ola</h1>
+    </div>
+    <Modal
+      title="Adicionar banana à página inicial"
+      image={banana}
+      onClickBanner={() => alert('hey bro')}
+    />
+
+    <style jsx>{`
+      .content {
+        height: 2000px;
+        background-color: red;
+      }
+    `}</style>
   </div>
 );
 
